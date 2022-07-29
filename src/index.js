83420979a4f1cf2de9c59ccb52ad6c36a024bc52
Naztex <nazteck@yandex.ru>
2022-07-29 17:27:35 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let brackets = bracketsConfig.join('').replace(/,/g, '');
+	let arrayCount = [];
+
+	for (let element of str) {
+			let index = brackets.indexOf(element)
+
+			if (index % 2 === 0) {
+				
+				if (element === brackets[index + 1] && arrayCount[arrayCount.length - 1] === index){
+						arrayCount.pop();
+				} else if (element === brackets[index + 1] && arrayCount[arrayCount.length - 1] !== index) {
+						arrayCount.push(index)
+				}
+				else{
+						arrayCount.push(index)
+				}
+
+			} else {
+				if (arrayCount.pop() !== index-1){
+						return false;
+				}
+			}
+			
+		}
+	return arrayCount.length === 0;
 }
